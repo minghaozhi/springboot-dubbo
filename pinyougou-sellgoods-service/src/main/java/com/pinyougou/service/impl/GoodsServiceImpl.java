6d8af8a128e151b4ee6a11176954934fc67bc682
minghaozhi <597575122@qq.com>
2019-02-25 18:26:37 +0800

    框架搭建

--- pinyougou-sellgoods-service/src/main/java/com/pinyougou/service/impl/GoodsServiceImpl.java
+++ pinyougou-sellgoods-service/src/main/java/com/pinyougou/service/impl/GoodsServiceImpl.java
@@ -1,7 +1,7 @@
 package com.pinyougou.service.impl;
 
 import com.pinyougou.service.GoodsService;
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.Service;
 
 /**
  * @program: pinyougou-parent
